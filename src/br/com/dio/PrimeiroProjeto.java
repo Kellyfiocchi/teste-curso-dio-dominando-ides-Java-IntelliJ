ee63707c2438b8ef93d9e161d5eb20959aefcdbf
Kellyfiocchi <fiocchi1989_faculdade@outlook.com>
2022-07-15 14:41:02 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroProjeto.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroProjeto {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello Word! " + (a+b));*/
+    }
+}
