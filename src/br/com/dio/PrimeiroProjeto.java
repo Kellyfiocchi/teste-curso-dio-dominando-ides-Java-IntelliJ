25933525392057fc9d0a608c31b0d3bc9c408d1b
Kellyfiocchi <fiocchi1989_faculdade@outlook.com>
2022-07-15 15:46:16 -0300

    criando class livro

--- src/br/com/dio/PrimeiroProjeto.java
+++ src/br/com/dio/PrimeiroProjeto.java
@@ -8,10 +8,45 @@ public class PrimeiroProjeto {
 
         System.out.println(gato);
 
+        Livro livro = new Livro("Storytelling com dados", 256);
+        System.out.println(livro);
 
 
         /*int a = 5;
         int b = 3;
         System.out.println("Hello Word! " + (a+b));*/
+    }
+}
+class Livro {
+   private String nome;
+   private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
